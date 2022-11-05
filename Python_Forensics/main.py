c71056f2fa0e131961af6838f4d8e2134bf266fd
huwenqiang Lenovo Xiaoxin Pro 16 <huwenqiang.hwq@protonmail.com>
2022-11-05 10:40:51 +0800

    Almost Completed

--- Python_Forensics/main.py
+++ Python_Forensics/main.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python
 #coding=utf-8
+
+# Test Command:
+# python .\main.py -p C:\Users\Common\AppData\Roaming\Mozilla\Firefox\Profiles\e375hkoh.default-esr > ./output.txt
+
+
 import re
 import optparse
 import os
@@ -10,6 +15,8 @@ import urllib.parse
 import io
 import sys
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='gb18030')
+
+CUSTOMIZED_KEYWORD = []
 
 # Parse the file downloads.sqlite and output the download record
 def print_downloads(downloadDB):
@@ -104,43 +111,43 @@ def print_search_engine(placesDB):
         url = str(row[0])
         date = str(row[1])
         if 'duckduckgo' in url.lower():
-            r = re.findall(r'wd=.*?\&', url)
+            r = re.findall(r'q=.*?\&', url)
             if r:
                 search=r[0].split('&')[0]
-                search=search.replace('wd=', '').replace('+', ' ')
-                print ('[+] ' + date + ' - Searched For: ' + search)
+                search=search.replace('q=', '').replace('+', ' ')
+                print ('[+] ' + date + ' - Searched Duckduckgo For: ' + search)
     
     # Yandex
     for row in c:
         url = str(row[0])
         date = str(row[1])
         if 'yandex' in url.lower():
-            r = re.findall(r'wd=.*?\&', url)
+            r = re.findall(r'text=.*?\&', url)
             if r:
                 search=r[0].split('&')[0]
-                search=search.replace('wd=', '').replace('+', ' ')
-                print ('[+] ' + date + ' - Searched For: ' + search)
+                search=search.replace('text=', '').replace('+', ' ')
+                print ('[+] ' + date + ' - Searched Yandex For: ' + search)
 
     # Startpage
     for row in c:
         url = str(row[0])
         date = str(row[1])
         if 'startpage' in url.lower():
-            r = re.findall(r'wd=.*?\&', url)
+            r = re.findall(r'q=.*?\&', url)
             if r:
                 search=r[0].split('&')[0]
-                search=search.replace('wd=', '').replace('+', ' ')
-                print ('[+] ' + date + ' - Searched For: ' + search)
+                search=search.replace('q=', '').replace('+', ' ')
+                print ('[+] ' + date + ' - Searched Startpage For: ' + search)
 
     # Sougou
     for row in c:
         url = str(row[0])
         date = str(row[1])
-        if 'sougou' in url.lower():
-            r = re.findall(r'wd=.*?\&', url)
+        if 'sogou' in url.lower():
+            r = re.findall(r'query=.*?\&', url)
             if r:
                 search=r[0].split('&')[0]
-                search=search.replace('wd=', '').replace('+', ' ')
+                search=search.replace('query=', '').replace('+', ' ')
                 print ('[+] ' + date + ' - Searched For: ' + search)
 
     # WikiPedia
@@ -169,7 +176,6 @@ def print_bookmark(placesDB):
         if 'google' in title.lower():
             print ('[+] ' + title)
     print ("\n")
-    
     print ("-- Bookmark URLs contains keyword \"google:\" --")
     c.execute("select * from moz_places")
     for row in c:
@@ -189,12 +195,12 @@ def print_bookmark(placesDB):
         if '百度' in title.lower():
             print ('[+] ' + title)
     print ("\n")
-    
     print ("-- Bookmark URLs contains keyword \"baidu:\" --")
     c.execute("select * from moz_places")
     for row in c:
         url = str(row[1])
         if 'baidu' in url.lower():
+            url = urllib.parse.unquote(url)
             print ('[+] ' + url)
     print ("\n")
 
@@ -208,7 +214,6 @@ def print_bookmark(placesDB):
         if '必应' in title.lower():
             print ('[+] ' + title)
     print ("\n")
-    
     print ("-- Bookmark URLs contains keyword \"bing:\" --")
     c.execute("select * from moz_places")
     for row in c:
@@ -217,14 +222,103 @@ def print_bookmark(placesDB):
             url = urllib.parse.unquote(url)
             print ('[+] ' + url)
     print ("\n")
+    
+    # Wikipedia
+    print ("-- Bookmark names contains keyword \"Wikipedia:\" or \"维基\" --")
+    c.execute("select title from moz_bookmarks")
+    for row in c:
+        title = str(row[0])
+        if 'wikipedia' in title.lower():
+            print ('[+] ' + title)
+        if '维基' in title.lower():
+            print ('[+] ' + title)
+    print ("\n")
+    
+    print ("-- Bookmark URLs contains keyword \"wikipedia:\" --")
+    c.execute("select * from moz_places")
+    for row in c:
+        url = str(row[1])
+        if 'wikipedia' in url.lower():
+            url = urllib.parse.unquote(url)
+            print ('[+] ' + url)
+    print ("\n")
+
+    # mail
+    print ("-- Bookmark names contains keyword \"mail\" or \"邮箱\" --")
+    c.execute("select title from moz_bookmarks")
+    for row in c:
+        title = str(row[0])
+        if 'mail' in title.lower():
+            print ('[+] ' + title)
+        if '邮箱' in title.lower():
+            print ('[+] ' + title)
+    print ("\n")
+    
+    print ("-- Bookmark URLs contains keyword \"mail:\" --")
+    c.execute("select * from moz_places")
+    for row in c:
+        url = str(row[1])
+        if 'mail' in url.lower():
+            url = urllib.parse.unquote(url)
+            print ('[+] ' + url)
+    print ("\n")
+
+    # VPN
+    print ("-- Bookmark names contains keyword \"VPN\" --")
+    c.execute("select title from moz_bookmarks")
+    for row in c:
+        title = str(row[0])
+        if 'vpn' in title.lower():
+            print ('[+] ' + title)
+    print ("\n")
+    
+    print ("-- Bookmark URLs contains keyword \"vpn:\" --")
+    c.execute("select * from moz_places")
+    for row in c:
+        url = str(row[1])
+        if 'vpn' in url.lower():
+            url = urllib.parse.unquote(url)
+            print ('[+] ' + url)
+    print ("\n")
     return
+
+
+def bookmark_customized_keywords(placesDB, customized_keyword = CUSTOMIZED_KEYWORD):
+    conn = sqlite3.connect(placesDB)
+    c = conn.cursor()
+    print ("-- Bookmark names contains customized Keywords --")
+    c.execute("select title from moz_bookmarks")
+    for row in c:
+        title = str(row[0])
+        for i in customized_keyword:
+            i = i.lower()
+            if i in title.lower():
+                print ('[+] ' + title)
+    print ("\n")
+    print ("-- Bookmark URLs contains customized keyword --")
+    c.execute("select * from moz_places")
+    for row in c:
+        url = str(row[1])
+        for i in customized_keyword:
+            i = i.lower()
+            if i in url.lower():
+                url = urllib.parse.unquote(url)
+                print ('[+] ' + url)
+    print ("\n")
+
+    return   
+
 
 
 def main():
     parser = optparse.OptionParser("[*]Usage: firefoxParse.py -p <firefox profile path> ")
-    parser.add_option('-p', dest='pathName', type='string', help='specify skype profile path')
+    parser.add_option('-p', dest = 'pathName', type = 'string', help = 'Specify Firefox Profile Path')
+    parser.add_option('-c', dest = 'custom_keyword', help = 'Specify Custom Keyword Dictionary')
     (options, args) = parser.parse_args()
+
     pathName = options.pathName
+    custom_keyword_place = options.custom_keyword
+
     if pathName == None:
         print (parser.usage)
         exit(0)
@@ -250,8 +344,19 @@ def main():
             print_history(placesDB)
             print_search_engine(placesDB)
             print_bookmark(placesDB)
+
+            if custom_keyword_place == None:
+                pass
+            elif os.path.isfile(custom_keyword_place) == False:
+                print ('[!] Path Does Not Exist: ' + custom_keyword_place)
+            else:
+                with open (custom_keyword_place) as keyword_file:
+                    custom_keyword = keyword_file.readlines()
+                    custom_keyword = [line.rstrip() for line in custom_keyword]
+                    bookmark_customized_keywords(placesDB, custom_keyword)
         else:
             print ('[!] PlacesDb does not exist: ' + placesDB)
+
 
 if __name__ == '__main__':
     main()
